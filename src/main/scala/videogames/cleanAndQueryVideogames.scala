4260b48b432a5c56bec9fd89de73adf3214050e4
Julia Blanco <julia.blanco@bosonit.com>
2023-08-28 00:46:49 +0200

    nuevas busquedas en videogames dataset

--- src/main/scala/videogames/cleanAndQueryVideogames.scala
+++ src/main/scala/videogames/cleanAndQueryVideogames.scala
@@ -3,6 +3,7 @@ package videogames
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions.Window
 
 object cleanAndQueryVideogames {
 
@@ -61,8 +62,9 @@ object cleanAndQueryVideogames {
       .agg(count("players").as("ocurrencias"))
       .orderBy("players")
       .show(100, truncate = false)
+
     //Agrupar para minimizar el número de valores distintos.
-    //Lo hago con el método .equalTo en lugar de con === y en una columna nueva para poder hacer después otras transformaciones sobre players
+    //Lo hago con el método .equalTo en lugar de con == y en una columna nueva para poder hacer después otras transformaciones sobre players
 
     val gamesDf2 = gamesDf1.withColumn("maxplayers",
       when(col("players").equalTo("1 Player"), "1")
@@ -126,7 +128,6 @@ object cleanAndQueryVideogames {
             .or(col("players").equalTo("Massively Multiplayer")), ">64")
         .otherwise(col("players"))
     )
-
     gamesDf2.show(truncate = false)
 
     //Añado una columna booleana que dé información sobre si el juego es Online o no. Uso expresión regular con .rlike (igual que en HIVE HQL)
@@ -146,7 +147,67 @@ object cleanAndQueryVideogames {
     gamesDf3.show(25, truncate = false)
     gamesDf3.printSchema()
 
-    //HACER EXPLODE DE GENRES Y LUEGO PIVOT PARA SABER CUÁNTOS GAMES DE CADA GÉNERO HA HECHO CADA DESARROLLADOR.
+    //¿Cuántos videojuegos de cada género ha creado cada desarrollador
+    //   Utilizo explode() y pivot() y lo hago sobre una partición:
+    //   (En este dataFrame se ve que aún queda mucho trabajo por hacer de limpieza, por ejemplo hay un género llamado "online")
+    val gamesDfParticion =gamesDf3
+      .repartition(col("name"), col("developer"), col("genre"), col("score"), col("user score"))
+    gamesDfParticion.show(9, truncate = false)
+
+    val explodedGenre = gamesDfParticion
+      .select(col("name"),
+        col("developer"),
+        explode(col("genre")).as("genre"),
+        col("score"),
+        col("user score"))
+
+    val tablaGeneros = explodedGenre
+      .groupBy(col("developer"))
+      .pivot("genre")
+      .agg(avg("score").as("avg score"))
+      .orderBy("developer")
+    tablaGeneros.show(30, truncate = false)
+
+    //¿Cuál es el desarrollador que crea los juegos de lucha (Fighting) más valorados?
+    tablaGeneros
+      .select("developer", "Fighting")
+      .orderBy(desc("Fighting"))
+      .show(5, truncate = false)
+
+    gamesDf3.agg(min(col("r-date"))).show()
+    //Ideas para usar Window:
+    //¿Cuál es el mejor juego de cada año según la puntuación de Metacritic y según los usuarios?
+    val ventanayear = Window.partitionBy(year(col("r-date")))
+    val bestgamesDf = gamesDf3
+      .withColumn("year", year(col("r-date")))
+      .withColumn("max score", max("score").over(ventanayear))
+      .withColumn("max user score", max("user score").over(ventanayear))
+      .filter(col("score")
+        .equalTo(col("max score"))
+        .or(col("user score")
+          .equalTo(col("max user score"))))
+      .select("year", "name", "score", "user score")
+      .orderBy(col("year"), desc("score"), desc("user score"))
+
+    bestgamesDf.show(60, truncate = false)
+
+    //Total de usuarios para cada desarrollador y cada plataforma con una función window
+    val ventanausers = Window.partitionBy("name", "platform")
+    val mostplayedgamesDf = gamesDf3
+      .withColumn("total users", sum("users").over(ventanausers))
+      .orderBy(desc("total users"))
+    mostplayedgamesDf.show(15, truncate = false)
+
+    //Los 10 juegos en los que hay más diferencia entre la puntuación de Metacritic y la del usuario.
+    val controversialgamesDf = gamesDf3
+      .withColumn("year", year(col("r-date")))
+      .withColumn("score difference", abs(col("score") - col("user score")))
+      .select("name", "year", "score", "user score", "score difference")
+      .orderBy(desc("score difference"))
+
+    controversialgamesDf.show(10, truncate = false)
+
+
     /*
     val tablaContingencia = gamesDf3.filter(col("DESC_DISTRITO").isin("CENTRO", "BARAJAS", "RETIRO"))
       .groupBy(col("COD_EDAD_INT"))
